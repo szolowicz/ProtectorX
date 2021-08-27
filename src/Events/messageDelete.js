0eb9a22d66c2fcc0f72ae42b2f226bbc979114d5
Adrian <lepekadrian123@gmail.com>
2021-08-27 02:17:25 +0200

    feat(messageDelete.js): add event messageDelete.js

---
+++ src/Events/messageDelete.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js")
+const Event = require(`../Structures/Event.js`)
+
+module.exports = new Event("messageDelete", (client, message) => {
+  const LogChannel = client.channels.cache.find(c => c.name == "║🤖║bot-test")
+  
+  if (!LogChannel) return
+
+  const DeletedLog = new Discord.MessageEmbed()
+  
+  DeletedLog.setTitle("`✖️🗑️ Deleted Message`")
+    .setColor("RANDOM")
+    .setThumbnail(message.author.displayAvatarURL({dynamic: true}))
+    .addFields(
+      {
+        name: "👀 Deleted by",
+        value: `${message.author} **|** \`ID : ${message.author.id}\``,
+        inline: false
+      },
+      {
+        name: "👀 On the channel",
+        value: `${message.channel.name} **|** \`ID : ${message.channel.id}\``,
+        inline: false
+      },
+      {
+        name: "👀 Deleted message",
+        value: message.content,
+        inline: false
+      }
+    )
+    .setTimestamp()
+  
+  LogChannel.send({ embeds: [DeletedLog] })
+})
