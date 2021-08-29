22eb03a9560b365ffd30ffc0b1cc44070430bb26
Adrian <lepekadrian123@gmail.com>
2021-08-29 02:24:49 +0200

    feat(slowmode.js): add command slowmode

---
+++ src/Commands/slowmode.js
@@ -0,0 +1,18 @@
+const Command = require(`../Structures/Command.js`)
+
+module.exports = new Command({
+  name: "slowmode",
+  description: "Set slowmode send messages channel!",
+  permission: "ADMINISTRATOR",
+
+  async run(message, args, client) {
+    const time = args[1]
+
+    if (!time || isNaN(time))
+      return message.reply(`\`${time == undefined ? "✖️ Enter a time in seconds!" : `❌ ${time} is an invalid number!`}\``)
+
+    message.channel.setRateLimitPerUser(time, "No Reason")
+
+    message.channel.send(`\`❎ Successfully set the slowmode on this channel ${time} seconds!\``)
+  }
+})
