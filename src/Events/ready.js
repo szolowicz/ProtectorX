f7010b425744f6fd1b7da8ce9a52823288d6981d
Adrian <lepekadrian123@gmail.com>
2021-09-01 00:28:06 +0200

    feat(ready.js): add changing bot status

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -2,4 +2,19 @@ const Event = require(`../Structures/Event.js`)
 
 module.exports = new Event("ready", (client) => {
   console.log("Bot is online!")
+
+  const arrayOfStatus = [
+    "🤖 Author : Adriks",
+    "💻 GitHub : https://github.com/AdriksOwy",
+    "My prefix is ❓",
+    "❔help - All bot commands."
+  ]
+
+  let index = 0
+  setInterval(() => {
+    if (index === arrayOfStatus.length) index = 0
+    const status = arrayOfStatus[index]
+    client.user.setActivity(status)
+    index++
+  }, 3000)
 })
