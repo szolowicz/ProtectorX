36f71b4f7f04d333e7a38f30f400da2bb146ee31
Adrian <lepekadrian123@gmail.com>
2021-09-01 17:52:14 +0200

    chore(messageCreate.js): cleaning code

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,15 +1,11 @@
 const Event = require(`../Structures/Event.js`)
 
 module.exports = new Event("messageCreate", (client, message) => {
-  if (message.author.bot) return
-
-  if (!message.content.startsWith(client.prefix)) return
+  if (!message.content.startsWith(client.prefix) || message.author.bot || message.content == "?") return
 
   const args = message.content.substring(client.prefix.length).split(/ +/)
 
   const command = client.commands.find(cmd => cmd.name == args[0])
-
-  if (message.content == "?") return message.reply("`✖️ This is my prefix!`")
 
   if (!command) return message.reply("`❌ Invalid command!`")
 
