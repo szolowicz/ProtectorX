91c087ab3330ffd77668642054fd75757809f5ec
Adrian <lepekadrian123@gmail.com>
2021-09-01 17:55:53 +0200

    chore(help.js): update message

--- src/Commands/help.js
+++ src/Commands/help.js
@@ -12,7 +12,7 @@ module.exports = new Command({
     embed.setTitle("`✖️📋 Commands list`")
       .setColor("BLUE")
       .setDescription(
-        "**?help** - `Shows all commands ProtectorX.`\n**?ping** - `Checks the ping of the bot.`\n**?clear** - `Clears the entered number of messages.`\n**?nuke** - `Clears channel from messages.`\n**?github** - `Shows the author's github.`\n**?ship** - `Matchmaking.`")
+        "**?help** - `Shows all commands ProtectorX.`\n**?ping** - `Checks the ping of the bot.`\n**?github** - `Shows the author's github.`\n**?ship** - `Matchmaking.`\n**?slowmode** - `Sets the cooldown writing on the channel.`\n**?clear** - `Clears the entered number of messages.`\n**?nuke** - `Clears channel from messages.`\n**?kick** - `Kicks a player from the server.`\n**?ban** - `Bans a player from the server.`\n**?unban** - `Unbans a player from the server.`")
       .setTimestamp()
     
     message.channel.send({ embeds: [embed] })
